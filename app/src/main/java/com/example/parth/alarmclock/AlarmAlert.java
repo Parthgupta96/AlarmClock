ffbb416f690111a62bd5e301e1b7069db825a4e8
Priyanshu Goyal <priyanshu96.goyal@gmail.com>
2016-04-11 11:36:21 +0530

    MultiAlarms Added and Answer Input Implemented

--- app/src/main/java/com/example/parth/alarmclock/AlarmAlert.java
+++ app/src/main/java/com/example/parth/alarmclock/AlarmAlert.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -20,7 +21,9 @@ public class AlarmAlert extends AppCompatActivity {
     public MediaPlayer mediaPlayer;
     Vibrator vibrator;
     Alarm alarm;
+    TextView answer;
     private final String LOG_TAG = AlarmAlert.class.getSimpleName();
+    String answerString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +56,69 @@ public class AlarmAlert extends AppCompatActivity {
             long[] pattern = {1000, 200, 200, 200};
             vibrator.vibrate(pattern, 0);
         }
+
+        answer = (TextView) findViewById(R.id.answer);
+    }
+
+    public void button1(View view){
+        answerString = answerString + "1";
+        answer.setText(answerString);
+    }
+
+    public void button2(View view){
+        answerString = answerString + "2";
+        answer.setText(answerString);
+    }
+
+    public void button3(View view){
+        answerString = answerString + "3";
+        answer.setText(answerString);
+    }
+
+    public void button4(View view){
+        answerString = answerString + "4";
+        answer.setText(answerString);
+    }
+
+    public void button5(View view){
+        answerString = answerString + "5";
+        answer.setText(answerString);
+    }
+
+    public void button6(View view){
+        answerString = answerString + "6";
+        answer.setText(answerString);
+    }
+
+    public void button7(View view){
+        answerString = answerString + "7";
+        answer.setText(answerString);
+    }
+
+    public void button8(View view){
+        answerString = answerString + "8";
+        answer.setText(answerString);
+    }
+
+    public void button9(View view){
+        answerString = answerString + "9";
+        answer.setText(answerString);
+    }
+
+    public void button0(View view){
+        answerString = answerString + "0";
+        answer.setText(answerString);
+    }
+
+    public void submit(View view){
+        if(answerString.equals("23")){
+            closeAlarm();
+        }
+    }
+
+    public void clear(View view){
+        answerString = answerString.substring(0,answerString.length()-1);
+        answer.setText(answerString);
     }
 
     protected void unlockScreen(){
@@ -64,7 +130,7 @@ public class AlarmAlert extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     }
 
-    public void closeAlarm(View view){
+    public void closeAlarm(){
         Log.v(LOG_TAG, "will now stop");
         mediaPlayer.stop();
         if(vibrator!=null)
